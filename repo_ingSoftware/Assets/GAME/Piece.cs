d7b559504ad6d16a5d1b3d4c55ea6e2833d5c40c
pokelocos <nare.rare.fire@gmail.com>
2020-08-12 00:50:07 -0400

    final

--- repo_ingSoftware/Assets/GAME/Piece.cs
+++ repo_ingSoftware/Assets/GAME/Piece.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Piece : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class Piece : MonoBehaviour
 
     private void OnMouseDown()
     {
-        GameplayManager.Select(this);
+        //GameplayManager.Select(this);
+        SceneManager.LoadScene("PieceViewer");
     }
 }
