55bbe6978b799c668548c581e7eff58c55a79a3e
pokelocos <nare.rare.fire@gmail.com>
2020-08-11 22:39:38 -0400

    autofocus y boton para regresar al menu principal

---
+++ repo_ingSoftware/Assets/AutoFocus.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class AutoFocus : MonoBehaviour
+{
+    private bool mVuforiaStarted = false;
+    private bool mFlashEnabled = false;
+
+    private void Start()
+    {
+        VuforiaARController vuforia = VuforiaARController.Instance;
+
+        if(vuforia != null)
+        {
+            vuforia.RegisterVuforiaStartedCallback(StartAfterVuforia);
+        }
+    }
+
+    private void StartAfterVuforia()
+    {
+        mVuforiaStarted = true;
+        SetAutofocus();
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if(!pause)
+        {
+            if(mVuforiaStarted)
+            {
+                SetAutofocus();
+            }
+        }
+    }
+
+    private void SetAutofocus()
+    {
+        if(CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO))
+        {
+            Debug.Log("AutoFocus set");
+        }
+        else
+        {
+            Debug.Log("no autofocus supported");
+        }
+    }
+
+}
