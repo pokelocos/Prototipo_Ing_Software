1e5b471bcc82f1e37382126d22fb53c1328ed34c
pokelocos <nare.rare.fire@gmail.com>
2020-08-12 00:09:51 -0400

    search """"

--- repo_ingSoftware/Assets/GAME/DataBase/MuseumInfo.cs
+++ repo_ingSoftware/Assets/GAME/DataBase/MuseumInfo.cs
@@ -1,6 +1,15 @@
-﻿namespace DataBase
+﻿using Boo.Lang;
+using System.IO;
+
+namespace DataBase
 {
     public class MuseumInfo
     {
+        private string name;
+        private string descrition;
+        //private Location location;
+        //private List<NumberPhone> contacs;
+        private List<string> Pieces;
+        //private File file;
     }
 }
---
+++ repo_ingSoftware/Assets/InfoPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanel : MonoBehaviour
+{
+    public Text nameDisplay;
+    public Text descriptionDisplay;
+    public Image imageDisplay;
+
+    public void SetInfo(string name, string description,Sprite img)
+    {
+        nameDisplay.text = name;
+        descriptionDisplay.text = description;
+        imageDisplay.sprite = img;
+    }
+
+}
